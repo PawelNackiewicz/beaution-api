7566ce9dc4e990bf6df7aec174876ee56c0b3b57
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2023-05-30 13:01:51 +0200

    remove fastiofy cli

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
-'use strict'
+const fastify = require('fastify')({
+  logger: true
+})
 
-const path = require('path')
-const AutoLoad = require('@fastify/autoload')
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
 
-// Pass --options via CLI arguments in command to enable these options.
-module.exports.options = {}
-
-module.exports = async function (fastify, opts) {
-  // Place here your custom code!
-
-  // Do not touch the following lines
-
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
-
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
 }
+start()
